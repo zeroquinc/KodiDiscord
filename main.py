4c314eb6955107ee76b8996c5f65b4dd07c647f7
zeroquinc <desiler@protonmail.com>
2024-01-02 04:28:03 +0100

    Refactor update_rpc functions

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def loading(string):
         print(b, end="\r")
         time.sleep(1)
 
-
 def set_rp(info, length):
     global previous_info, previous_speed
     info = info['result']['item']
@@ -46,77 +45,100 @@ def set_rp(info, length):
     image_url = get_image_url(tmdb_id, media_type)
 
     if info['type'] == 'movie':
-        if length['speed'] == 0:
-            print(f"Updated RPC - Paused movie \n{info['title']}")
-            RPC.update(details=str(info['title']),
-                       state="Paused...",
-                       large_image=image_url,
-                       large_text='Watching a movie on Kodi',
-                       small_image='pause',
-                       small_text='Paused')
-        else:
-            print(f"Updated RPC - Playing movie \n{info['title']}")
-            RPC.update(details=str(info['title']),
-                       start=start_time,
-                       end=end_time,
-                       large_image=image_url,
-                       large_text='Watching a movie on Kodi',
-                       small_image='play',
-                       small_text='Playing')
-        previous_info = info
-        previous_speed = length['speed']
-
-    if info['type'] == "episode":
-        state_info = get_state_info(info)
-        if length['speed'] == 0:
-            print(f"Updated RPC - Paused episode \n{info['showtitle']} {state_info}")
-            RPC.update(state=state_info,
-                       details=str(info['showtitle']),
-                       large_image=image_url,
-                       large_text='Watching a TV Show on Kodi',
-                       small_image='pause',
-                       small_text='Paused')
-        else:
-            print(f"Updated RPC - Playing episode \n{info['showtitle']} {state_info}")
-            RPC.update(state=state_info,
-                       details=str(info['showtitle']),
-                       start=start_time,
-                       end=end_time,
-                       large_image=image_url,
-                       large_text='Watching a TV Show on Kodi',
-                       small_image='play',
-                       small_text='Playing')
-        previous_info = info
-        previous_speed = length['speed']
-            
-    if info['type'] == "channel":
-        title = get_title(info)
-        if length['speed'] == 0:
-            print(f"Updated RPC - Paused channel \n{info['label']}")
-            RPC.update(state=title,
-                       details=str(info['label']),
-                       large_image=image_url,
-                       large_text='Watching Live TV on Kodi',
-                       small_image='pause',
-                       small_text='Paused')
-        else:
-            print(f"Updated RPC - Playing channel \n{info['label']}")
-            RPC.update(state=title,
-                       details=str(info['label']),
-                       start=start_time,
-                       end=end_time,
-                       large_image=image_url,
-                       large_text='Watching Live TV on Kodi',
-                       small_image='play',
-                       small_text='Playing')
-        previous_info = info
-        previous_speed = length['speed']
-
-    if info['type'] == 'unknown' and length['speed'] == 0:
+        update_rpc_movie(info, length, start_time, end_time, image_url)
+    elif info['type'] == 'episode':
+        update_rpc_episode(info, length, start_time, end_time, image_url)
+    elif info['type'] == 'channel':
+        update_rpc_channel(info, length, start_time, end_time, image_url)
+    elif info['type'] == 'unknown' and length['speed'] == 0:
         print("Nothing is playing. Clearing RPC...")
         RPC.clear()
 
+    previous_info = info
+    previous_speed = length['speed']
+
     time.sleep(15)
+
+def update_rpc_movie(info, length, start_time, end_time, image_url):
+    if length['speed'] == 0:
+        update_rpc_paused_movie(info, image_url)
+    else:
+        update_rpc_playing_movie(info, start_time, end_time, image_url)
+
+def update_rpc_episode(info, length, start_time, end_time, image_url):
+    if length['speed'] == 0:
+        update_rpc_paused_episode(info, image_url)
+    else:
+        update_rpc_playing_episode(info, start_time, end_time, image_url)
+
+def update_rpc_channel(info, length, start_time, end_time, image_url):
+    if length['speed'] == 0:
+        update_rpc_paused_channel(info, image_url)
+    else:
+        update_rpc_playing_channel(info, start_time, end_time, image_url)
+
+def update_rpc_paused_movie(info, image_url):
+    print(f"Updated RPC - Paused movie \n{info['title']}")
+    RPC.update(details=str(info['title']),
+               state="Paused...",
+               large_image=image_url,
+               large_text='Watching a movie on Kodi',
+               small_image='pause',
+               small_text='Paused')
+
+def update_rpc_playing_movie(info, start_time, end_time, image_url):
+    print(f"Updated RPC - Playing movie \n{info['title']}")
+    RPC.update(details=str(info['title']),
+               start=start_time,
+               end=end_time,
+               large_image=image_url,
+               large_text='Watching a movie on Kodi',
+               small_image='play',
+               small_text='Playing')
+
+def update_rpc_paused_episode(info, image_url):
+    state_info = get_state_info(info)
+    print(f"Updated RPC - Paused episode \n{info['showtitle']} {state_info}")
+    RPC.update(state=state_info,
+               details=str(info['showtitle']),
+               large_image=image_url,
+               large_text='Watching a TV Show on Kodi',
+               small_image='pause',
+               small_text='Paused')
+
+def update_rpc_playing_episode(info, start_time, end_time, image_url):
+    state_info = get_state_info(info)
+    print(f"Updated RPC - Playing episode \n{info['showtitle']} {state_info}")
+    RPC.update(state=state_info,
+               details=str(info['showtitle']),
+               start=start_time,
+               end=end_time,
+               large_image=image_url,
+               large_text='Watching a TV Show on Kodi',
+               small_image='play',
+               small_text='Playing')
+
+def update_rpc_paused_channel(info, image_url):
+    title = get_title(info)
+    print(f"Updated RPC - Paused channel \n{info['label']}")
+    RPC.update(state=title,
+               details=str(info['label']),
+               large_image=image_url,
+               large_text='Watching Live TV on Kodi',
+               small_image='pause',
+               small_text='Paused')
+
+def update_rpc_playing_channel(info, start_time, end_time, image_url):
+    title = get_title(info)
+    print(f"Updated RPC - Playing channel \n{info['label']}")
+    RPC.update(state=title,
+               details=str(info['label']),
+               start=start_time,
+               end=end_time,
+               large_image=image_url,
+               large_text='Watching Live TV on Kodi',
+               small_image='play',
+               small_text='Playing')
 
 def calculate_start_time(length):
     return (datetime.now() - timedelta(hours=length["time"]['hours'], minutes=length["time"]['minutes'], seconds=length["time"]['seconds'])).timestamp()
