f76ba02f4b353bde5ad6991e28c43851e21c6e4b
zeroquinc <desiler@protonmail.com>
2024-01-29 09:59:02 +0100

    Refactor: Letterboxd class

--- src/letterboxd.py
+++ src/letterboxd.py
@@ -1,11 +1,16 @@
 from .custom_logger import logger
 
-"""	
-This module contains functions related to Letterboxd.
-"""
+class Letterboxd:
+    """	
+    This class contains methods related to Letterboxd.
+    """
 
-# Function to get the Letterboxd URL of a media
-def get_letterboxd_url(tmdb_id):
-    url = f"https://letterboxd.com/tmdb/{tmdb_id}" if tmdb_id else None
-    logger.debug(f"Letterboxd URL: {url}")
-    return url
+    def __init__(self, tmdb_id):
+        self.tmdb_id = tmdb_id
+        self.url = self.get_letterboxd_url()
+
+    # Method to get the Letterboxd URL of a media
+    def get_letterboxd_url(self):
+        url = f"https://letterboxd.com/tmdb/{self.tmdb_id}" if self.tmdb_id else None
+        logger.debug(f"Letterboxd URL: {url}")
+        return url
