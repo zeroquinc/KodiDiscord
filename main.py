c991c98f5381bd8549dcaa864264cae70c95e240
zeroquinc <desiler@protonmail.com>
2024-01-29 10:20:39 +0100

    Refactor: KodiMonitor class

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import requests
 import time
-
 from src.custom_logger import logger
 from src.rpc import fetch_info, fetch_length, update_rp
 
-"""
-This file contains the main function of the program. Run this to start the program.
-"""
+class KodiMonitor:
+    def __init__(self):
+        self.last_info = None
+        self.last_length = None
+        self.last_time = None
+        self.session = requests.Session()
 
-# Main function
-def main():
-    try:
-        with requests.Session() as session:
-            last_info, last_length, last_time = None, None, None
+    def run(self):
+        try:
             while True:
-                info = fetch_info(session)
-                length = fetch_length(session)
+                info = fetch_info(self.session)
+                length = fetch_length(self.session)
                 if info is None or length is None:  # If either info or length is None, continue to the next iteration
                     time.sleep(3)
                     continue
-                if info != last_info or length != last_length or (length['result']['speed'] == 1 and length['result']['time'] == last_time):
+                if info != self.last_info or length != self.last_length or (length['result']['speed'] == 1 and length['result']['time'] == self.last_time):
                     update_rp(info, length)  # Update the RP if there's new information
-                    last_info, last_length, last_time = info, length, last_time  # Update the last info and length
+                    self.last_info, self.last_length, self.last_time = info, length, length['result']['time']  # Update the last info and length
                 time.sleep(3)  # Always pause for 3 seconds between iterations
-    except KeyboardInterrupt:
-        logger.info("Program interrupted by user. Exiting...")
+        except KeyboardInterrupt:
+            logger.info("Program interrupted by user. Exiting...")
 
 if __name__ == "__main__":
-    main()
+    monitor = KodiMonitor()
+    monitor.run()
