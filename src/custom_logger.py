1987b7565a81de62fcc56fe3ab37bdb1bc0d4cfb
zeroquinc <desiler@protonmail.com>
2024-05-04 12:45:16 +0200

    chore: optimize logging readability

--- src/custom_logger.py
+++ src/custom_logger.py
@@ -3,19 +3,30 @@ import sys
 
 from config import LOG_LEVEL
 
-"""
-This file contains the custom logger.
-"""
-
-# Function to switch the logger according to the LOG_LEVEL variable
 def switch_logger():
+    """
+    Switch the logger configuration based on the LOG_LEVEL variable.
+
+    Explanation:
+    This function sets up the logger configuration based on the value of the LOG_LEVEL variable. It removes the existing logger configuration and adds a new one with specific settings determined by the LOG_LEVEL value.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: If the LOG_LEVEL is not 'DEBUG' or 'INFO'.
+    """
     logger.remove()
-    if LOG_LEVEL == 'DEBUG':
-        logger.add(sys.stdout, level='DEBUG', colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <red>{file}</red> | <yellow>{function}</yellow> | <white>{level}</white> | <level>{message}</level>")
-    elif LOG_LEVEL == 'INFO':
-        logger.add(sys.stdout, level='INFO', colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <white>{level}</white> | <level>{message}</level>")
+    log_configs = {
+        'DEBUG': {"level": 'DEBUG', "colorize": True, "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <red>{file}</red> | <yellow>{function}</yellow> | <white>{level}</white> | <level>{message}</level>"},
+        'INFO': {"level": 'INFO', "colorize": True, "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <white>{level}</white> | <level>{message}</level>"}
+    }
+    if LOG_LEVEL in log_configs:
+        logger.add(sys.stdout, **log_configs[LOG_LEVEL])
     else:
         raise ValueError("Invalid log level")
 
-# Call the function to set the logger according to the LOG_LEVEL variable
 switch_logger()
