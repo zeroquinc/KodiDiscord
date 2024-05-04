5a1f8b677eb8a7e43ae79bc27483a91f4c73a7ff
zeroquinc <desiler@protonmail.com>
2024-05-04 13:07:45 +0200

    chore: letterboxd.py improve code readability

--- src/letterboxd.py
+++ src/letterboxd.py
@@ -1,11 +1,15 @@
 from .custom_logger import logger
 
-"""	
-This module contains functions related to Letterboxd.
-"""
-
-# Function to get the Letterboxd URL of a media
 def get_letterboxd_url(tmdb_id):
+    """
+    Constructs the Letterboxd URL for a given TMDB ID.
+
+    Parameters:
+    - tmdb_id: The Movie Database (TMDB) ID of the media.
+
+    Returns:
+    - The Letterboxd URL string if tmdb_id is provided, else None.
+    """
     url = f"https://letterboxd.com/tmdb/{tmdb_id}" if tmdb_id else None
     logger.debug(f"Letterboxd URL: {url}")
     return url
